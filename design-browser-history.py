65024f17d57f38155d0ef75edf1d1b91c42f9737
nhinhdao <ndao2803@gmail.com>
2024-12-28 16:52:31 -0500

    Designed browser history

--- design-browser-history.py
+++ design-browser-history.py
@@ -9,40 +9,30 @@ class Node():
         nextVal = self.next.val if self.next else "None"
         print("curr " + self.val + " /prev " + prevVal+ " /next " + nextVal)
 
-
 class BrowserHistory(object):
     def __init__(self, homepage):
-        self.currentUrl = Node(homepage)
-        print("Initial Val " + self.currentUrl.val)
-
-    def visit(self, url):
-        # create a new node
-        newNode = Node(url, self.currentUrl)
-        curr = self.currentUrl
-        # point self to new node
-        self.currentUrl.next = newNode
-        # reset self
-        self.currentUrl = self.currentUrl.next
-        self.currentUrl.printSelf()
+        self.currentPage = Node(homepage, None, None)
         
+    def visit(self, url):
+        page = Node(url, self.currentPage, None)
+        self.currentPage.next = page
+        self.currentPage = page
 
     def back(self, steps):
-        while steps > 0 and self.currentUrl.prev:
-            self.currentUrl = self.currentUrl.prev
+        while steps > 0:
+            lastPage = self.currentPage.prev
+            self.currentPage = lastPage if lastPage else self.currentPage
             steps -= 1
-        
-        self.currentUrl.printSelf()
-        return self.currentUrl.val
+        return self.currentPage.val
         
 
     def forward(self, steps):
-        self.currentUrl.printSelf()
-        while steps > 0 and self.currentUrl.next:
-            self.currentUrl = self.currentUrl.next
+        while steps > 0:
+            nextPage = self.currentPage.next
+            self.currentPage = nextPage if nextPage else self.currentPage
             steps -= 1
-        
-        return self.currentUrl.val
-
+        return self.currentPage.val
+    
 browserHistory = BrowserHistory("leetcode.com")
 browserHistory.visit("google.com")       # You are in "leetcode.com". Visit "google.com"
 browserHistory.visit("facebook.com")     # You are in "google.com". Visit "facebook.com"
@@ -55,5 +45,5 @@ browserHistory.forward(2)                # You are in "linkedin.com", you cannot
 browserHistory.back(2)                   # You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
 browserHistory.back(7)                   # You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
 
-print(browserHistory.currentUrl.val)
+print(browserHistory.currentPage.val)
 # [null,null,null,null,"facebook.com","google.com","facebook.com",null,"linkedin.com","google.com","leetcode.com"]
