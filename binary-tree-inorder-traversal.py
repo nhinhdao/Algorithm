a67585711747c6a5c5260a9356f8c049d7ea5f78
nhinhdao <ndao2803@gmail.com>
2025-01-11 18:46:16 -0500

    added binary tree inorder traversal

--- binary-tree-inorder-traversal.py
+++ binary-tree-inorder-traversal.py
@@ -5,17 +5,35 @@ class TreeNode(object):
         self.left = left
         self.right = right
 
-        
+
 class Solution(object):
     def __init__(self):
         self.arr = []
 
-    def inorderTraversal(self, root):
+    def inorderTraversalUsingRecursion(self, root):
         if root is None:
             return self.arr
 
         self.inorderTraversal(root.left)
         self.arr.append(root.val)
         self.inorderTraversal(root.right)
+
+        return self.arr
+    
+    def inorderTraversal(self, root):
+        stack = []
+        node = root
+
+        while stack or node:
+            while node:
+                # traverse left, append node to stack
+                stack.append(node)
+                node = node.left
+            
+            # end of left node (node is now NONE)
+            node = stack.pop()
+            self.arr.append(node.val)
+            # traverse to right
+            node = node.right
 
         return self.arr
